6213620392b4254b451fa12fd596c6565f98a677
muhammad-13-2004 <muhammadabdullah13112004@gmail.com>
2025-04-19 00:44:41 -0700

    Courses

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 import Login from './Components/Login'
 import Register from './Components/Register'
 
-=======
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
 import Courses from './Components/Courses'
->>>>>>> 4772818 (Courses)
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,11 +23,9 @@ function App() {
           <Route path="/course/:id" element={<CourseDetail />} /> */}
         </Routes>
       </Router>
-=======
       <Hero/>
       <Navbar/>
       <Courses/>
->>>>>>> 4772818 (Courses)
     </>
   )
 }
