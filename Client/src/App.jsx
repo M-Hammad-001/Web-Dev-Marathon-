77c24998b4eec3fb818b981741f603b58a1f454f
Muhammad Saleh <salehshazli786@gmail.com>
2025-04-19 11:19:22 +0500

    Added components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './Components/Hero'
 
 function App() {
 
   return (
     <>
-      <Hero/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/course/create" element={<CreateCourse />} />
+          <Route path="/course/:id" element={<CourseDetail />} />
+        </Routes>
+      </Router>
     </>
   )
 }
